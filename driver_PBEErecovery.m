e1539d8e07abf340908db456d3bf1cfcc1a93f5d
Cook <dtc2@NIST.GOV>
2022-03-10 11:08:30 -0800

    Seperate driver and main file
    removes logic from driver script that faciliates methods into a seperate function titled main_functionality.m

--- driver_PBEErecovery.m
+++ driver_PBEErecovery.m
@@ -4,7 +4,8 @@
 % Input data consists of building model info and simulated component-level
 % damage and conesequence data for a suite of realizations, likely assessed
 % as part of a FEMA P-58 analysis. Inputs are read in as matlab variables
-% direclty from matlab data files.
+% direclty from matlab data files, as well as loaded from csvs in the
+% static_tables directory.
 
 % Output data is saved to a specified outputs directory and is saved into a
 % single matlab variable as at matlab data file.
@@ -21,11 +22,6 @@ model_name = 'ICSB'; % Name of the model;
 model_dir = ['inputs' filesep 'example_inputs']; % Directory where the simulated inputs are located
 outputs_dir = ['outputs' filesep model_name]; % Directory where the assessment outputs are saved
 
-%% Import Packages
-import recovery.repair_schedule.main_repair_schedule
-import recovery.functionality.main_functionality
-import recovery.impedance.main_impeding_factors
-
 %% Load FEMA P-58 performance model data and simulated damage and loss
 load([model_dir filesep model_name filesep 'simulated_inputs.mat'])
 
@@ -34,23 +30,11 @@ systems = readtable(['static_tables' filesep 'systems.csv']);
 subsystems = readtable(['static_tables' filesep 'subsystems.csv']);
 impeding_factor_medians = readtable(['static_tables' filesep 'impeding_factors.csv']);
 
-%% Combine compoment attributes into recovery filters to expidite recovery assessment
-[damage.fnc_filters] = fn_preprocessing(damage.comp_ds_table);
-
-%% Simulate ATC 138 Impeding Factors
-[functionality.impeding_factors] = main_impeding_factors(damage, impedance_options, ...
-    damage_consequences.repair_cost_ratio, damage_consequences.inpsection_trigger, ...
-    systems, building_model.building_value, impeding_factor_medians, regional_impact.surge_factor); 
-
-%% Construct the Building Repair Schedule
-[damage, functionality.worker_data, functionality.building_repair_schedule ] = ...
-    main_repair_schedule(damage, building_model, damage_consequences.red_tag, ...
-    repair_time_options, systems, functionality.impeding_factors, regional_impact.surge_factor);
-
-%% Calculate the Recovery of Building Reoccupancy and Function
-[ functionality.recovery ] = main_functionality( damage, building_model, ...
-    damage_consequences, functionality.utilities, functionality_options, ...
-    tenant_units, subsystems );
+%% Run Recovery Method
+[functionality] = main_PBEErecovery(damage, damage_consequences, ...
+    building_model, tenant_units, systems, subsystems, impedance_options, ...
+    impeding_factor_medians,  regional_impact, repair_time_options, ...
+    functionality, functionality_options);
 
 %% Save Outputs
 if ~exist(outputs_dir,'dir')
---
+++ main_PBEErecovery.m
@@ -0,0 +1,69 @@
+function [functionality] = main_PBEErecovery(damage, damage_consequences, ...
+    building_model, tenant_units, systems, subsystems, impedance_options, ...
+    impeding_factor_medians,  regional_impact, repair_time_options, ...
+    functionality, functionality_options)
+% Perform the ATC-138 functional recovery time assessement given similation
+% of component damage for a single shaking intensity
+%
+% Parameters
+% ----------
+% damage: struct
+%   contains per damage state damage and loss data for each component in the building
+% damage_consequences: struct
+%   data structure containing simulated building consequences, such as red
+%   tags and repair costs ratios
+% building_model: struct
+%   general attributes of the building model
+% tenant_units: table
+%   attributes of each tenant unit within the building
+% systems: table
+%   data table containing information about each system's attributes
+% subsystems: table
+%   attributes of building subsystems; data provided in static tables
+%   directory
+% impedance_options: struct
+%   general impedance assessment user inputs such as mitigation factors
+% impeding_factor_medians: table
+%   median delays for various impeding factors
+% regional_impact.surge_factor: number
+%   amplification in impedance time due to regional constraints on labor
+%   and materials
+% repair_time_options: struct
+%   general repair time options such as mitigation factors
+% functionality.utilities
+%   data structure containing simulated utility downtimes
+% functionality_options: struct
+%   recovery time optional inputs such as various damage thresholds
+%
+%
+% Returns
+% -------
+% functionality: struct
+%   contains data on the recovery of tenant- and building-level function, 
+%   recovery trajectorires, and contributions from systems and components, 
+%   simulated repair schedule breakdowns and impeding times.
+
+%% Import Packages
+import recovery.impedance.main_impeding_factors
+import recovery.repair_schedule.main_repair_schedule
+import recovery.functionality.main_functionality
+
+%% Combine compoment attributes into recovery filters to expidite recovery assessment
+[damage.fnc_filters] = fn_preprocessing(damage.comp_ds_table);
+
+%% Simulate ATC 138 Impeding Factors
+[functionality.impeding_factors] = main_impeding_factors(damage, impedance_options, ...
+    damage_consequences.repair_cost_ratio, damage_consequences.inpsection_trigger, ...
+    systems, building_model.building_value, impeding_factor_medians, regional_impact.surge_factor); 
+
+%% Construct the Building Repair Schedule
+[damage, functionality.worker_data, functionality.building_repair_schedule ] = ...
+    main_repair_schedule(damage, building_model, damage_consequences.red_tag, ...
+    repair_time_options, systems, functionality.impeding_factors, regional_impact.surge_factor);
+
+%% Calculate the Recovery of Building Reoccupancy and Function
+[ functionality.recovery ] = main_functionality( damage, building_model, ...
+    damage_consequences, functionality.utilities, functionality_options, ...
+    tenant_units, subsystems );
+
+end
