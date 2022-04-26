7c3f9e5944274e81ac2e0de8a464653eb6be8fb4
Cook <dtc2@NIST.GOV>
2022-04-26 15:12:25 -0700

    Add logic to also prioritize reoccupancy repairs
    in the repair schedule priority algorthim, method will now first
    prioritize systems that can affect reoccupancy, then remaining systems
    that still affect function, then other repairs

--- +recovery/+repair_schedule/fn_prioritize_systems.m
+++ +recovery/+repair_schedule/fn_prioritize_systems.m
@@ -1,7 +1,8 @@
 function [ sys_idx_priority_matrix ] = fn_prioritize_systems( systems, damage, tmp_repair_complete_day )
 % Determine the priority of worker allocation for each system and realization
 % based on default table priorities, whether they have the potential to 
-% affect function and whether they are resolved by temporary repairs.
+% affect reoccupancy or function and whether they are resolved by 
+% temporary repairs.
 
 % Parameters
 % ----------
@@ -34,6 +35,12 @@ function [ sys_idx_priority_matrix ] = fn_prioritize_systems( systems, damage, t
 num_sys = height(systems);
 [num_reals, ~] = size(damage.tenant_units{1}.qnt_damaged);
 
+% Find which components potentially affect reoccupancy accross any tenant unit
+affects_reoccupancy = zeros(num_reals, height(damage.comp_ds_table));
+for s = 1:length(damage.tenant_units)
+    affects_reoccupancy = affects_reoccupancy | (damage.fnc_filters.affects_reoccupancy & damage.tenant_units{s}.qnt_damaged);
+end
+
 % Find which components potentially affect function accross any tenant unit
 affects_function = zeros(num_reals, height(damage.comp_ds_table));
 for s = 1:length(damage.tenant_units)
@@ -44,16 +51,22 @@ end
 tmp_repaired = tmp_repair_complete_day < inf; % inf here means there is not temp repair
    
 %% Define ranks for each system 
+sys_affects_reoccupancy = zeros(num_reals, num_sys); % only prioritize the systems that potentially affect function
 sys_affects_function = zeros(num_reals, num_sys); % only prioritize the systems that potentially affect function
 sys_tmp_repaired = zeros(num_reals, num_sys); % dont prioitize the systems that are completely resolved by temporary repairs
 for sys = 1:num_sys
     sys_filter = damage.comp_ds_table.system' == sys;
+    sys_affects_reoccupancy(:,sys) = any(affects_reoccupancy(:,sys_filter),2); % any damage that potentially affects reoccupancy in this system
     sys_affects_function(:,sys) = any(affects_function(:,sys_filter),2); % any damage that potentially affects function in this system
     sys_tmp_repaired(:,sys) = all(tmp_repaired(:,sys_filter),2); % all components must be resolved by temp repairs in this system
 end
-prioritize_system = sys_affects_function & ~sys_tmp_repaired;
+prioritize_system_reoccupancy = sys_affects_reoccupancy & ~sys_tmp_repaired;
+prioritize_system_function_only = sys_affects_function & ~prioritize_system_reoccupancy & ~sys_tmp_repaired;
+non_priorities = ~prioritize_system_reoccupancy & ~prioritize_system_function_only;
 
-sys_priority_matrix = prioritize_system .* (100 + systems.priority') + ~prioritize_system .* (200 + systems.priority'); % the added 200 is just to deprioritize non-function hindering damage
+sys_priority_matrix = prioritize_system_reoccupancy   .* (100 + systems.priority') + ... % First prioritize reoccpancy repairs (100 classifies first priority bank, important for sort function below)
+                      prioritize_system_function_only .* (200 + systems.priority') + ... % then priotize repairs that only affect function (200 classifies second priority bank)
+                      non_priorities                  .* (300 + systems.priority'); % finally, repair the systems that dont affect reoccupancy or function (300 classifies third priority bank)
 
 % use rank matrix to get row indeces of priorities
 [~, sys_idx_priority_matrix] = sort(sys_priority_matrix, 2);
